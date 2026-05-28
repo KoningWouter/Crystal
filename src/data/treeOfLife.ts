571c6130a88ab070dfcc716ce4ad7ef26446a5e7
Wouter Rengelink <wouterr@gmail.com>
2026-05-28 08:11:58 +0200

    Adjust Levensboom positions - Keter lower for text space, balanced vertical spacing

--- src/data/treeOfLife.ts
+++ src/data/treeOfLife.ts
@@ -4,17 +4,17 @@
 // Verticale proportionering - Tiferet en Yesod lager voorAleph/Mem verbindingen
 
 export const sefirot = [
-  { id: 'keter', name: 'Keter', x: 50, y: 5 },       // Kroon - bovenaan
-  { id: 'binah', name: 'Binah', x: 20, y: 16 },      // Begrip - LINKS boven
-  { id: 'chokhmah', name: 'Chokhmah', x: 80, y: 16 }, // Wijsheid - RECHTS boven
-  { id: 'da_at', name: "Da'at", x: 50, y: 22 },      // Kennis - korte afstand van Keter
-  { id: 'gevurah', name: 'Gevurah', x: 20, y: 36 },  // Strengheid - LINKS midden (omhoog)
-  { id: 'chesed', name: 'Chesed', x: 80, y: 36 },     // Liefde - RECHTS midden (omhoog)
-  { id: 'tiferet', name: 'Tiferet', x: 50, y: 48 },   // Schoonheid - lager dan Gevurah/Chesed
+  { id: 'keter', name: 'Keter', x: 50, y: 10 },      // Kroon - bovenaan (iets omlaag voor tekst-ruimte)
+  { id: 'binah', name: 'Binah', x: 20, y: 18 },     // Begrip - LINKS boven
+  { id: 'chokhmah', name: 'Chokhmah', x: 80, y: 18 }, // Wijsheid - RECHTS boven
+  { id: 'da_at', name: "Da'at", x: 50, y: 24 },     // Kennis - korte afstand van Keter
+  { id: 'gevurah', name: 'Gevurah', x: 20, y: 38 },  // Strengheid - LINKS midden
+  { id: 'chesed', name: 'Chesed', x: 80, y: 38 },    // Liefde - RECHTS midden
+  { id: 'tiferet', name: 'Tiferet', x: 50, y: 50 },   // Schoonheid - centraal
   { id: 'hod', name: 'Hod', x: 20, y: 62 },           // Majesteit - LINKS onder
   { id: 'netzach', name: 'Netzach', x: 80, y: 62 },   // Overwinning - RECHTS onder
-  { id: 'yesod', name: 'Yesod', x: 50, y: 68 },       // Fundament - lager dan Hod/Netzach
-  { id: 'malkuth', name: 'Malkuth', x: 50, y: 90 },   // Koninkrijk - onderaan
+  { id: 'yesod', name: 'Yesod', x: 50, y: 66 },       // Fundament - centraal
+  { id: 'malkuth', name: 'Malkuth', x: 50, y: 88 },   // Koninkrijk - onderaan
 ]
 
 // Alle 25 paden met exacte Hebreeuwse letters
